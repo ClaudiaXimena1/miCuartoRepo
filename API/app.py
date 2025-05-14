c226aba1ccf044991a9092335291c2b4e7b22689
Claudia Bustamante <claudiaximena1@hotmail.com>
2025-05-14 16:32:32 -0500

    Método de platzi agregado

--- API/app.py
+++ API/app.py
@@ -11,12 +11,6 @@ def get_familia():
     return rows
 
 
-@app.get("/superheroesDC")
-def get_superheroes():
-    rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
-    return rows
-
-
 @app.get("/cursosPlatzi")
 def get_cursos():
     rows = ["Docker", "Bash", "Linux", "Inglés", "Python", "Javascript", "Shazam", "Azure"]
